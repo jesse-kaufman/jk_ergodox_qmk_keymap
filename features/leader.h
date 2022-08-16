5ecca4b51d571c8d83ee5eb1a6722aaa2fd10723
Jesse Kaufman <glandix@lloydnet.org>
2022-08-16 17:03:16 -0500

    Include tapdance.h

--- features/leader.h
+++ features/leader.h
@@ -1,6 +1,7 @@
 #include QMK_KEYBOARD_H
 #include "../definitions/layers.h"
 #include "../definitions/keycodes.h"
+#include "../features/tapdance.h"
 #include "lighting.h"
 
 bool did_leader_succeed;
