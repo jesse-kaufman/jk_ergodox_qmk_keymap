b3061da57c833083108e7f3f0a0a7e317f2ba78d
Jesse Kaufman <glandix@lloydnet.org>
2022-09-20 07:48:18 -0500

    Tweak leader setup

--- features/leader.c
+++ features/leader.c
@@ -8,77 +8,80 @@ void my_leader_matrix_scan_user(void) {
 
 		leader_start();
 
+		// Finder
+		SEQ_ONE_KEY(KC_F) {
+			tap_code16(HYPR(KC_D));
+			did_leader_succeed = true;
+		}
+
+
 		// Terminal
 		SEQ_ONE_KEY(KC_T) {
 			tap_code16(HYPR(KC_T));
 			did_leader_succeed = true;
-		}else
+		}
 
-			// Safari
-			SEQ_ONE_KEY(KC_S) {
-				tap_code16(HYPR(KC_S));
-				did_leader_succeed = true;
-			}else
+		// Safari
+		SEQ_ONE_KEY(KC_S) {
+			tap_code16(HYPR(KC_S));
+			did_leader_succeed = true;
+		}
 
-			// Notes
-			SEQ_TWO_KEYS(KC_N, KC_O) {
-				tap_code16(HYPR(KC_N));
-				did_leader_succeed = true;
-			}else
+		// Notes
+		SEQ_TWO_KEYS(KC_N, KC_O) {
+			tap_code16(HYPR(KC_N));
+			did_leader_succeed = true;
+		}
 
-			// Mail
-			SEQ_TWO_KEYS(KC_M, _KC_A) {
-				tap_code16(HYPR(KC_M));
-				did_leader_succeed = true;
-			}else
+		// Mail
+		SEQ_TWO_KEYS(KC_M, KC_A) {
+			tap_code16(HYPR(KC_M));
+			did_leader_succeed = true;
+		}
 
-			// Finder
-			SEQ_ONE_KEY(KC_F) {
-				tap_code16(HYPR(KC_D));
-				did_leader_succeed = true;
-			}else
 
-			// Messenger (FB)
-			SEQ_TWO_KEYS(KC_F, KC_B) {
-				tap_code16(HYPR(KC_F));
-				did_leader_succeed = true;
-			}else
+		// Messenger (FB)
+		SEQ_TWO_KEYS(KC_F, KC_B) {
+			tap_code16(HYPR(KC_F));
+			did_leader_succeed = true;
+		}
 
-			// Messages
-			SEQ_TWO_KEYS(KC_S, KC_M) {
-				tap_code16(HYPR(KC_B));
-				did_leader_succeed = true;
-			}else
+		// Messages
+		SEQ_TWO_KEYS(KC_S, KC_M) {
+			tap_code16(HYPR(KC_B));
+			did_leader_succeed = true;
+		}
 
-			// Terminal
-			SEQ_TWO_KEYS(KC_T, KC_E) {
-				tap_code16(HYPR(KC_E));
-				did_leader_succeed = true;
-			}else
+		// Safari Technology Edition
+		SEQ_TWO_KEYS(KC_T, KC_E) {
+			tap_code16(HYPR(KC_E));
+			did_leader_succeed = true;
+		}
 
-			// Chrome
-			SEQ_TWO_KEYS(KC_C, KC_H) {
-				tap_code16(HYPR(KC_G));
-				did_leader_succeed = true;
-			}else
+		// Chrome
+		SEQ_TWO_KEYS(KC_C, KC_H) {
+			tap_code16(HYPR(KC_G));
+			did_leader_succeed = true;
+		}
 
-			// Lightroom
-			SEQ_TWO_KEYS(KC_L, KC_R) {
-				tap_code16(HYPR(KC_L));
-				did_leader_succeed = true;
-			}else
+		// Lightroom
+		SEQ_TWO_KEYS(KC_L, KC_R) {
+			tap_code16(HYPR(KC_L));
+			did_leader_succeed = true;
+		}
 
-			// Reminders
-			SEQ_TWO_KEYS(KC_R, KC_E) {
-				tap_code16(HYPR(KC_R));
-				did_leader_succeed = true;
-			}else
+		// Reminders
+		SEQ_TWO_KEYS(KC_R, KC_E) {
+			tap_code16(HYPR(KC_R));
+			did_leader_succeed = true;
+		}
 
-			// Music
-			SEQ_ONE_KEY(_PLAY) {
-				tap_code16(HYPR(KC_MEDIA_PLAY_PAUSE));
-				did_leader_succeed = true;
-			}
+		// Music
+		SEQ_TWO_KEYS(KC_M, KC_U) {
+			tap_code16(HYPR(KC_U));
+			did_leader_succeed = true;
+		}
+
 
 		leader_end();
 	}
