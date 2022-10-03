aceda74c8a7d37159df018ad160db19c7b6c1944
Jesse Kaufman <glandix@lloydnet.org>
2022-10-03 08:20:26 -0500

    Send all leader strings as a chain of HYPER commands for Keyboard Maestro

--- features/leader.c
+++ features/leader.c
@@ -10,88 +10,8 @@ void my_leader_matrix_scan_user(void) {
 
 		leader_start();
 
-		// Finder
-		SEQ_ONE_KEY(KC_F) {
-			tap_code16(HYPR(KC_D));
-			did_leader_succeed = true;
-		}
-
-		// Terminal
-		SEQ_ONE_KEY(KC_T) {
-			tap_code16(HYPR(KC_T));
-			did_leader_succeed = true;
-		}
-
-		// Safari
-		SEQ_ONE_KEY(KC_S) {
-			tap_code16(HYPR(KC_S));
-			did_leader_succeed = true;
-		}
-
-		// Notes
-		SEQ_TWO_KEYS(KC_N, KC_O) {
-			tap_code16(HYPR(KC_N));
-			did_leader_succeed = true;
-		}
-
-		// Mail
-		SEQ_TWO_KEYS(KC_M, KC_A) {
-			tap_code16(HYPR(KC_M));
-			did_leader_succeed = true;
-		}
-
-		// FB Messenger
-		SEQ_TWO_KEYS(KC_F, KC_B) {
-			tap_code16(HYPR(KC_F));
-			did_leader_succeed = true;
-		}
-
-		// Messages
-		SEQ_TWO_KEYS(KC_M, KC_E) {
-			tap_code16(HYPR(KC_B));
-			did_leader_succeed = true;
-		}
-
-		// Safari Technology Edition
-		SEQ_TWO_KEYS(KC_T, KC_E) {
-			tap_code16(HYPR(KC_E));
-			did_leader_succeed = true;
-		}
-
-		// Chrome
-		SEQ_TWO_KEYS(KC_C, KC_H) {
-			tap_code16(HYPR(KC_G));
-			did_leader_succeed = true;
-		}
-
-		// Lightroom
-		SEQ_TWO_KEYS(KC_L, KC_R) {
-			tap_code16(HYPR(KC_L));
-			did_leader_succeed = true;
-		}
-
-		// Reminders
-		SEQ_TWO_KEYS(KC_R, KC_E) {
-			tap_code16(HYPR(KC_R));
-			did_leader_succeed = true;
-		}
-
-		// Music
-		SEQ_TWO_KEYS(KC_M, KC_U) {
-			tap_code16(HYPR(KC_U));
-			did_leader_succeed = true;
-		}
-
-		// Outlook
-		SEQ_TWO_KEYS(KC_O, KC_U) {
-			tap_code16(HYPR(KC_O));
-			did_leader_succeed = true;
-		}
-
-		// Photos
-		SEQ_THREE_KEYS(KC_P, KC_H, KC_O) {
-			tap_code16(HYPR(KC_P));
-			did_leader_succeed = true;
+		SEQ_ONE_KEY(KC_LEAD) {
+			tap_code16(LGUI(KC_SPACE));
 		}
 
 		leader_end();
@@ -113,7 +33,35 @@ void leader_end(void) {
 		// If no sequence was matched, did_leader_succeed will not
 		// have been set to true anywhere, so we'll end up here.
 		// Put your code for an unmatched leader key sequence here.
+		// send hotkey sequence
+
+
+		if (leader_sequence[0] >= KC_A && leader_sequence[0] <= KC_Z) {
+			register_code16(KC_HYPR);
+
+			tap_code16( leader_sequence[0] );
+
+			if ( leader_sequence[1]) {
+				tap_code16( leader_sequence[1] );
+
+				if (leader_sequence[2]) {
+					tap_code16( leader_sequence[2] );
+
+					if (leader_sequence[3]) {
+						tap_code16( leader_sequence[3] );
+
+						if (leader_sequence[4]) {
+							tap_code16( leader_sequence[4] );
+
+						}
+					}
+				}
+			}
+
+			unregister_code16(KC_HYPR);
+		}
 	}
+
 
 	ergodox_led_all_off();
 }
