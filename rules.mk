5d14c846fe44d97b64e82ce1e26cc8c3b2941642
Jesse Kaufman <glandix@lloydnet.org>
2022-10-07 18:59:33 -0500

    Tweak features

--- rules.mk
+++ rules.mk
@@ -1,22 +1,31 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 LTO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+LEADER_ENABLE = yes
+COMBO_ENABLE = yes
+KEYBOARD_SHARED_EP = yes
+
+
 CONSOLE_ENABLE = no
 COMMAND_ENABLE = no
 AUTO_SHIFT_ENABLE = no
 AUTO_SHIFT_MODIFIERS = no
-TAP_DANCE_ENABLE = yes
 SPACE_CADET_ENABLE = no
-CAPS_WORD_ENABLE = yes
+GRAVE_ESC_ENABLE = no
+MUSIC_ENABLE = no
+UNICODE_ENABLE = no
+KEY_OVERRIDE_ENABLE = no
+NKRO_ENABLE = no
+BOOTMAGIC_ENABLE = no
+MAGIC_ENABLE = no
+RING_BUFFERED_6KRO_REPORT_ENABLE = no
+MIDI_ENABLE = no
+
 SRC = matrix.c
 SRC += definitions/keycodes.c
 SRC += features/lighting.c
 SRC += features/leader.c
 SRC += features/tapdance.c
 SRC += features/combos.c
-GRAVE_ESC_ENABLE = no
-MUSIC_ENABLE = no
-LEADER_ENABLE = yes
-UNICODE_ENABLE = no
-COMBO_ENABLE = yes
-NKRO_ENABLE = no
