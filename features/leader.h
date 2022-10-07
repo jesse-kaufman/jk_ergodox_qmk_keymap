ed9a851739596421d9dcc15ad6aa402aa9224d15
Jesse Kaufman <glandix@lloydnet.org>
2022-10-07 18:59:11 -0500

    Add macro for leader keys

--- features/leader.h
+++ features/leader.h
@@ -2,3 +2,10 @@ bool did_leader_succeed;
 bool leading;
 
 void my_leader_matrix_scan_user(void);
+
+#define MY_KEY_SEQ(kc1,kc2,kc3,kc4,kc5,tap_kc) \
+	if (kc5) { SEQ_FIVE_KEYS(kc1,kc2,kc3,kc4,kc5) { tap_code16(tap_kc); did_leader_succeed = true; } } \
+	else if (kc4) { SEQ_FOUR_KEYS(kc1,kc2,kc3,kc4) { tap_code16(tap_kc); did_leader_succeed = true; } } \
+	else if (kc3) { SEQ_THREE_KEYS(kc1,kc2,kc3) { tap_code16(tap_kc); did_leader_succeed = true; } } \
+	else if (kc2) { SEQ_TWO_KEYS(kc1,kc2) { tap_code16(tap_kc); did_leader_succeed = true; } } \
+	else if (kc1) { SEQ_ONE_KEY(kc1) { tap_code16(tap_kc); did_leader_succeed = true; } };
