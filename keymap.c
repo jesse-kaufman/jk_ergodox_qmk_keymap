c4d1954d0b9bf9b6491336bbc9f88512746601c0
Jesse Kaufman <glandix@lloydnet.org>
2022-10-12 21:45:41 -0500

    Tweak layout

--- keymap.c
+++ keymap.c
@@ -16,58 +16,63 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	// COLEMAK BASE layer
 	[_BASE] = LAYOUT_ergodox_pretty(
 		TT(_FN),		___X___,		___X___,		SCMD(KC_4),		SCMD(KC_5),		LGUI(KC_S),	_PREV_DESK,													_DESKTOP,	 	___X___,		___X___,		___X___,		___X___,		___X___,		_TAB_MGMT,
-		MEH(KC_F13),	KC_Q,			KC_W,			KC_F,			KC_P,			KC_G,		KC_ESC,														TT(_NUM),		KC_J,			KC_L,			KC_U,			KC_Y,			KC_BSPACE,		LGUI(KC_M),
+		MEH(KC_F13),	KC_Q,			KC_W,			KC_F,			KC_P,			KC_G,		KC_ESC,														TT(_NUM),		KC_J,			KC_L,			KC_U,			KC_Y,			_MEH_LPRN,		LGUI(KC_M),
 		_APP_WINDOWS,	_KC_A,			_KC_R,			_KC_S,			_KC_T,			KC_D,																					KC_M,			KC_N,			_KC_E,			KC_I,			KC_O,			KC_PGUP,
-		_APP_TABS,		KC_Z,			KC_X,			KC_C,			KC_V,			KC_B,		KC_LEAD,													_HYPHEN,	 	_KC_K,			KC_H,			_COMMA,			_DOT,			KC_QUES,		KC_PGDOWN,
+		_APP_TABS,		KC_Z,			KC_X,			KC_C,			KC_V,			KC_B,		KC_LEAD,													OSL(_CODE),	 	_KC_K,			KC_H,			_COMMA,			_DOT,			KC_QUES,		KC_PGDOWN,
 		_UNDO,			_REDO,			_OSM_CTR,		_OSM_OPT,		_OSM_LSHIFT,																											OSL(_SYM),		_OSM_RCMD,		KC_DOWN,		KC_UP,			TT(_FN),
 																									_ZOOM_OUT,		_ZOOM_IN,					_VOL_DOWN,		_VOL_UP,
 																													MEH(KC_F19),				_PLAY,
 																						_SPACE,		_OSM_LCMD,		MEH(KC_F20),				KC_LEAD,		_OSM_LSHIFT,	_SHFT_ENTER
 	),
+
 	// SYM layer
 	[_SYM] = LAYOUT_ergodox_pretty(
 		_______,		_LDFQUO,		_RDFQUO,		_LFQUO,			_RFQUO,			___X___,		_______,												_______,		___X___,		___X___,		___X___,		___X___,		___X___,		_______,
-		_______,		___X___,		KC_AT,			KC_HASH,		KC_PERC,		___X___,		_______,												TO(_NUM),		KC_PIPE,		KC_UNDS,		KC_COLON,		KC_AMPR,		_______,		_______,
-		_______,		___X___,		KC_ASTR,		_HYPHEN,		KC_EQUAL,		KC_DLR,																					KC_BSLASH,		_LPRN,			_LBRACKET,		_LCURLBR,		_DQUOTE,		_______,
-		_______,		___X___,		KC_SLASH,		KC_PLUS,		KC_CIRC,		___X___,		_______,												TO(_CODE),		___X___,		_LTEQ,			_KC_SCOLN,		_GTEQ,			KC_SLASH,		_______,
+		_______,		___X___,		KC_AT,			KC_HASH,		KC_DLR,			KC_PERC,		_______,												TO(_NUM),		___X___,		KC_AMPR,		_LBRACKET,		KC_PIPE,		_MEH_LBRKT,		_______,
+		_______,		___X___,		KC_ASTR,		_HYPHEN,		KC_EQUAL,		KC_CIRC,																				___X___,		KC_LPRN,		KC_COLON,		KC_RPRN,		___X___,		_______,
+		_______,		___X___,		KC_SLASH,		KC_PLUS,		KC_UNDS,		___X___,		_______,												TO(_CODE),		___X___,		_LTEQ,			_KC_SCOLN,		_GTEQ,			___X___,		_______,
 		_______, 		_______,		_______,		_______,		_______,																												TO(_BASE),		_______,		_______,		_______,		TO(_BASE),
 																										_______,		_______,				_______,		KC_APP,
 																														_______,				_______,
-																						_______,		_______,		_______,				_______,		_______,		TO(_BASE)
+																						_______,		_______,		_______,				_______,		_______,		_______
 	),
-	// NUM layer
-	[_NUM] = LAYOUT_ergodox_pretty(
-		_______,		___X___,		___X___,		___X___,		___X___,		___X___,		_______,												_______,		___X___,		___X___,		___X___,		___X___,		___X___,		_______,
-		_______,		___X___,		_______,		_______,		_______,		___X___,		_______,												TO(_BASE),		___X___,		KC_7,			KC_8,			KC_9,			_______,		_______,
-		_______,		___X___,		KC_ASTR,		KC_MINUS,		KC_EQUAL,		___X___,																				___X___,		KC_4,			KC_5,			KC_6,			KC_PLUS,		_______,
-		_______,		___X___,		KC_SLASH,		KC_PLUS,		KC_MINUS,		___X___,		_______,												TO(_CODE),		KC_DOT,			KC_1,			KC_2,			KC_3,			KC_DOT,			_______,
-		_______,		_______,		_______,		_______,		_______,																												TO(_BASE),		_______,		_______,		_______,		TO(_BASE),
-																										_______,		_______,				_______,		_______,
-																														_______,				_______,
-																						_______,		_______,		_______,				_______,		_______,		KC_0
-	),
+
 	// CODE layer
 	[_CODE] = LAYOUT_ergodox_pretty(
 		_______,		___X___,		___X___,		___X___,		___X___,		___X___,		_______,												_______,		___X___,		___X___,		___X___,		___X___,		___X___,		_______,
-		_______,		___X___,		___X___,		_KC_COMMENT,	_KC_ML_COMMENT,	___X___,		_______,												TO(_NUM),		___X___,		___X___,		KC_TILD,		___X___,		_______,		_______,
-		_______,		___X___,		___X___,		_KC_NIX_HOME,	___X___,		___X___,																				___X___,		_MEH_LPRN,		_MEH_LBRKT,		_MEH_RBRKT,		___X___,		_______,
-		_______,		___X___,		___X___,		_KC_UP_DIR,		___X___,		___X___,		_______,												TO(_BASE),		___X___,		___X___ ,		KC_GRAVE,		___X___,		___X___,		_______,
+		_______,		___X___,		___X___,		_KC_COMMENT,	_KC_ML_COMMENT,	___X___,		_______,												TO(_NUM),		___X___,		KC_GRAVE,		KC_RBRACKET,	KC_TILD,		_MEH_RBRKT,		_______,
+		_______,		___X___,		___X___,		_KC_NIX_HOME,	___X___,		___X___,																				KC_BSLASH,		KC_LCBR,		_HTML,			KC_RCBR,		___X___,		_______,
+		_______,		___X___,		___X___,		_KC_UP_DIR,		___X___,		___X___,		_______,												TO(_BASE),		___X___,		___X___,		___X___,		___X___,		___X___,		_______,
 		_______,		_______,		_______,		_______,		_______,																												TO(_BASE),		_______,		_______,		_______,		TO(_BASE),
 																										_______,		_______,				_______,		_______,
 																														_______,				_______,
 																						_______,		_______,		_______,				_______,		_______,		_______
 	),
+
+	// NUM layer
+	[_NUM] = LAYOUT_ergodox_pretty(
+		_______,		___X___,		___X___,		___X___,		___X___,		___X___,		_______,												_______,		___X___,		___X___,		___X___,		___X___,		___X___,		_______,
+		_______,		___X___,		_______,		_______,		_______,		___X___,		_______,												TO(_BASE),		KC_ASTR,		KC_7,			KC_8,			KC_9,			KC_PLUS,		_______,
+		_______,		___X___,		KC_ASTR,		KC_MINUS,		KC_EQUAL,		___X___,																				KC_SLASH,		KC_4,			KC_5,			KC_6,			KC_MINUS,		_______,
+		_______,		___X___,		KC_SLASH,		KC_PLUS,		___X___,		___X___,		_______,												TO(_CODE),		KC_0,			KC_1,			KC_2,			KC_3,			KC_DOT,			_______,
+		_______,		_______,		_______,		_______,		_______,																												TO(_BASE),		_______,		_______,		_______,		TO(_BASE),
+																										_______,		_______,				_______,		_______,
+																														_______,				_______,
+																						_______,		_______,		_______,				_______,		_______,		_______
+	),
+
 	// FN layer
 	[_FN] = LAYOUT_ergodox_pretty(
 		_______,		___X___,		___X___,		___X___,		___X___,		___X___,		_______,												_______,		___X___,		___X___,		___X___,		___X___,		___X___,		_______,
-		_______,		___X___,		___X___,		___X___,		MEH(KC_P),		MEH(KC_5),		_______,												TO(_NUM),		_FORWARD,		KC_PGUP,		KC_UP,			___X___,		KC_HOME,		_______,
+		_______,		___X___,		___X___,		___X___,		MEH(KC_P),		MEH(KC_5),		_______,												TO(_NUM),		_______,		KC_PGUP,		KC_UP,			___X___,		KC_HOME,		_______,
 		_______,		___X___,		KC_LSFT,		MEH(KC_0),		MEH(KC_U),		MEH(KC_6),																				LALT(KC_LEFT),	KC_LEFT,		KC_DOWN,		KC_RIGHT,		LALT(KC_RIGHT),	_______,
-		_______,		___X___,		MEH(KC_X),		___X___,		MEH(KC_D),		MEH(KC_E),		_______,												TO(_CODE),		_BACK,			KC_PGDOWN,		___X___,		KC_DOT,			KC_END,			_______,
+		_______,		___X___,		MEH(KC_X),		___X___,		MEH(KC_D),		MEH(KC_E),		_______,												TO(_CODE),		___X___,		KC_PGDOWN,		___X___,		KC_DOT,			KC_END,			_______,
 		_______,		_______,		_______,		_______,		_______,																												TO(_BASE),		_______,		KC_LEFT,		KC_RIGHT,		TT(_MOUSE),
 																										_SCREEN_DN,		_SCREEN_UP,				_______,		_______,
 																														_______,				_______,
 																						_______,		_______,		_______,				_______,		_______,		_______
 	),
+
 	// MOUSE layer
 	[_MOUSE] = LAYOUT_ergodox_pretty(
 		_______,		___X___,		___X___,		___X___,		___X___,		___X___,		_______,												_______,		_______,		_______,		_______,		_______,		_______,		_______,
